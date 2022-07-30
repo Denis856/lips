4657788e6c8d02685d7517e76e98361a7c7767aa
Denis856 <102806191+Denis856@users.noreply.github.com>
2022-07-30 14:08:28 +0500

    fr3f4

--- main.php
+++ main.php
@@ -1,27 +1,17 @@
 <?php
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-
-$name = htmlspecialchars($name);
-$phone = htmlspecialchars($phone);
-
-$name = urldecode($name);
-$phone = urldecode($phone);
-
-$name = trim($name);
-$phone = trim($phone);
-
-echo $name;
-echo "<br>";
-echo $phone;
-
-mail("denis.malykhin.89@bk.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$phone ,"From: https://denis856.github.io/lips/ \r\n");
-
-if (mail("denis.malykhin.89@bk.ru", "Заказ с сайта", "ФИО:".$name.". E-mail: ".$phone ,"From: https://denis856.github.io/lips/ \r\n"))
- {
-    echo "сообщение успешно отправлено";
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("denis.malykhin.89@bk.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: https://denis856.github.io/lips/# \r\n"))
+ {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
-}
-
-?>
+}?>
