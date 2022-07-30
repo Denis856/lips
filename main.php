f8f8827e55b3097ee41ed9e7ef4ed0f4c7b70bf0
Denis856 <102806191+Denis856@users.noreply.github.com>
2022-07-30 13:47:41 +0500

    добавил php для отправки формы

---
+++ main.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+
+$name = urldecode($name);
+$phone = urldecode($phone);
+
+$name = trim($name);
+$phone = trim($phone);
+
+echo $name;
+echo "<br>";
+echo $phone;
+
+mail("denis.malykhin.89@bk.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: https://denis856.github.io/lips/ \r\n");
+
+if (mail("denis.malykhin.89@bk.ru", "Заказ с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: https://denis856.github.io/lips/ \r\n"))
+ {
+    echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+
+?>
