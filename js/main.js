4baa1f1b3a94ab4cb9132c254e93805840e7e8e4
Denis856 <102806191+Denis856@users.noreply.github.com>
2022-07-30 18:47:34 +0500

    csz

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 var swiper = new Swiper(".mySwiper", {});
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
